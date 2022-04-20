64febffe049f2d1198d465d8d9366f72fe50de0a
mskulski <skulak69@gmail.com>
2022-04-20 15:36:47 +0200

    Poprawione <div> kontemerem dla elementów

--- pobierz-dane-programisty/js/script.js
+++ pobierz-dane-programisty/js/script.js
@@ -2,19 +2,24 @@ $(document).ready(function () {
     $('#button').click(function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
             .done(function(data) {
-                let jqBody = $('body');
 
-                let dFirma = $('<div></div>').attr('id', 'dane-programisty').text(`Firma: ${data.firma}`);
-                let dImie = $('<div></div>').attr('id', 'dane-programisty').text(`Imie: ${data.imie}`);
-                let dNazwisko = $('<div></div>').attr('id', 'dane-programisty').text(`Nazwisko: ${data.nazwisko}`);
-                let dZawod = $('<div></div>').attr('id', 'dane-programisty').text(`Zawód: ${data.zawod}`);
+                let jqBody = $('body');
+                let jqItem = $('<div></div>').attr('id', 'dane-programisty');
+
+                let dFirma = $('<p></p>').attr('class', 'dane-programisty').text(`Firma: ${data.firma}`);
+                let dImie = $('<p></p>').attr('class', 'dane-programisty').text(`Imie: ${data.imie}`);
+                let dNazwisko = $('<p></p>').attr('class', 'dane-programisty').text(`Nazwisko: ${data.nazwisko}`);
+                let dZawod = $('<p></p>').attr('class', 'dane-programisty').text(`Zawód: ${data.zawod}`);
                 let hr = $('<hr>');
 
-                jqBody.append(dFirma);
-                jqBody.append(dImie);
-                jqBody.append(dNazwisko);
-                jqBody.append(dZawod);
-                jqBody.append(hr);
+
+                jqBody.append(jqItem);
+                
+                jqItem.append(dFirma);
+                jqItem.append(dImie);
+                jqItem.append(dNazwisko);
+                jqItem.append(dZawod);
+                jqItem.append(hr);
 
             });
     });
